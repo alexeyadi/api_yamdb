7ba911198d22a13f158bdc05b14649037e1963e9
Malyshev Timur <malyshevtimur00@gmail.com>
2022-11-05 00:09:17 +0300

    модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .validators import validation_year
 
 CHOICES = ('user', 'moderator', 'admin', )
 
@@ -8,13 +9,61 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True, )
     first_name = models.CharField(max_length=150, )
-    last_name = models.CharField(max_length= 150, )
+    last_name = models.CharField(max_length=150, )
     role = models.CharField(choices=CHOICES, default='user', max_length=50)
     bio = models.TextField('Биография', blank=True,)
 
-class Meta:
-    verbose_name = 'Пользователь'
-    verbose_name_plural = 'Пользователи'
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Имя жанра')
+    slug = models.SlugField(unique=True, verbose_name='Слаг жанра')
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True, verbose_name='Слаг категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    year = models.IntegerField(
+        validators=(validation_year,),
+        verbose_name='Год выпуска')
+    description = models.CharField(
+        max_length=150,
+        blank=True, null=True,
+        verbose_name='Описание')
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='title',
+        verbose_name='Жанр')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='title',
+        verbose_name='Категория',
+        null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name

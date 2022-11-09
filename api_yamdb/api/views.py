06f6197229ef451754080b828ad814734851594f
Алексей <aaamegulya@gmail.com>
2022-11-09 09:17:14 +0500

    correct pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -12,7 +12,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User
 from .filters import TitleFilter
 from .permissions import (IsAdmin, IsAdminModeratorAuthorPermission,
